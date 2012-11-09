ab16e3aeb5c6014adf3e3a93ac083a41c06a8291
Scott Little <scott@littleknownsoftware.com>
2012-11-09 18:12:18 +0100

    Add the standard script for the plugin to call.

---
+++ GetLatestIntoPlugin.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+#  UpdateInfoPlist.sh
+#  SignatureProfiler
+#
+#  Created by Scott Little on 9/11/12.
+#
+
+#	Ignore if we are cleaning
+if [[ -n $ACTION && $ACTION = "clean" ]]; then
+	echo "Ignoring during clean"
+	exit 0
+fi
+
+#	Set the locations
+export MY_UUID_REPO_NAME="MailMessageUUIDs"
+export MY_UUID_REPO="$SRCROOT/../$MY_UUID_REPO_NAME"
+
+#	Go into the MailMessagesUUIDs folder and ensure that it is up-to-date
+if [ ! -d "$MY_UUID_REPO" ]; then
+	echo "UUID Script ERROR - The $MY_UUID_REPO_NAME submodule doesn't exist!!"
+	exit 1
+fi
+cd "$MY_UUID_REPO"
+BRANCH=`git status | grep "branch" | cut -c 13-`
+IS_CLEAN=`git status | grep "nothing" | cut -c 1-17`
+if [ $BRANCH != "master" ]; then
+	echo "UUID Script ERROR - $MY_UUID_REPO_NAME needs to be on the master branch"
+	exit 2
+fi
+if [[ -z $IS_CLEAN || $IS_CLEAN != "nothing to commit" ]]; then
+	echo "UUID Script ERROR - $MY_UUID_REPO_NAME needs have a clean status"
+	exit 3
+fi
+git pull
+
+
+#	Run the script there that generates the UUID list file
+echo "Generating UUID list file"
+/usr/bin/osascript "ProcessMailMessageInfo.applescript"
+
+
+#	Run the other script that will update my Info.plist file
+echo "Updating Info.plist file"
+echo "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"
+/usr/bin/osascript "UpdateInfoPlist.applescript" "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"
