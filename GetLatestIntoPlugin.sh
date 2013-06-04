7d34ae037290e5760d10418b02e1560c7c1311ec
Scott Little <scott@littleknownsoftware.com>
2013-06-04 13:59:47 +0200

    Update script to only pull changes and rebuild the file when doing a release or when we have pulled a change manually. Helps keep the builds faster during regular development.

--- GetLatestIntoPlugin.sh
+++ GetLatestIntoPlugin.sh
@@ -7,7 +7,7 @@
 #
 
 #	Ignore if we are cleaning
-if [[ -n $ACTION && $ACTION = "clean" ]]; then
+if [[ -n $ACTION && "$ACTION" = "clean" ]]; then
 	echo "Ignoring during clean"
 	exit 0
 fi
@@ -24,23 +24,50 @@ if [ ! -d "$MY_UUID_REPO" ]; then
 	exit 1
 fi
 cd "$MY_UUID_REPO"
-git checkout -q master
-BRANCH=`git status | grep "# On branch" | cut -c 13-`
-if [ "$BRANCH" != "master" ]; then
-	echo "UUID Script ERROR - $MY_UUID_REPO_NAME needs to be on the master branch - I can't seem to change to it"
-	exit 2
+
+NEEDS_BUILD=1
+IS_RELEASE=0
+if [[ "$CONFIGURATION" == "Release" ]]; then
+	IS_RELEASE=1
 fi
-IS_CLEAN=`git status | grep "nothing" | cut -c 1-17`
-if [[ -z $IS_CLEAN || "$IS_CLEAN" != "nothing to commit" ]]; then
-	echo "UUID Script ERROR - $MY_UUID_REPO_NAME needs have a clean status"
-	exit 3
+if [[ -f "SupportableUUIDList.txt" && ($IS_RELEASE == 0) ]]; then
+	
+	#	If the latest supported file is newer than any local commits, go ahead and indicate that a build isn't needed
+	DATE_FORMAT="%a %b %d %T %Y"
+	LAST_PLIST_COMMIT_DATE=`git log -1 --format=%cd PlistFolder/*`
+	SUPPORTABLE_FILE_DATE=`python -c "import os,time; print time.ctime(os.path.getmtime('SupportableUUIDList.txt'))"`
+	COMMIT_DATE=`date -j -f "%a %b %d %T %Y" "$LAST_PLIST_COMMIT_DATE" +%s`
+	FILE_DATE=`date -j -f "%a %b %d %T %Y" "$SUPPORTABLE_FILE_DATE" +%s`
+	if [[ $FILE_DATE > $COMMIT_DATE ]]; then
+		NEEDS_BUILD=0
+	fi
+	
 fi
-git pull origin master
+
+# If we actually do need to rebuild the files
+if [[ $NEEDS_BUILD == 1 ]]; then
+	
+	echo "Ensuring that the files are up-to-date."
+
+	git checkout -q master
+	BRANCH=`git status | grep "# On branch" | cut -c 13-`
+	if [ "$BRANCH" != "master" ]; then
+		echo "UUID Script ERROR - $MY_UUID_REPO_NAME needs to be on the master branch - I can't seem to change to it"
+		exit 2
+	fi
+	IS_CLEAN=`git status | grep "nothing" | cut -c 1-17`
+	if [[ -z $IS_CLEAN || "$IS_CLEAN" != "nothing to commit" ]]; then
+		echo "UUID Script ERROR - $MY_UUID_REPO_NAME needs have a clean status"
+		exit 3
+	fi
+	git pull origin master
 
 
-#	Run the script there that generates the UUID list file
-echo "Generating UUID list file"
-/usr/bin/osascript "ProcessMailMessageInfo.applescript"
+	#	Run the script there that generates the UUID list file
+	echo "Generating UUID list file"
+	/usr/bin/osascript "ProcessMailMessageInfo.applescript"
+
+fi	# End of if we should rebuild
 
 
 #	Run the other script that will update my Info.plist file
