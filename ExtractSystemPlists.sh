d159e764bbd49027094f81dacbd76af0783bbb1c
Scott Little <scott@littleknownsoftware.com>
2013-11-18 13:19:56 +0100

    Update extraction script to give easier results.

--- ExtractSystemPlists.sh
+++ ExtractSystemPlists.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-OS_BUILD_VERSION=`defaults read /System/Library/CoreServices/SystemVersion.plist ProductBuildVersion`
-OS_VERSION=`defaults read /System/Library/CoreServices/SystemVersion.plist ProductVersion`
+OS_BUILD_VERSION=`sw_vers | grep BuildVersion | cut -f2 -s`
+OS_VERSION=`sw_vers | grep ProductVersion | cut -f2 -s`
 
 if [[ "$1" == "-d" ]]; then
-	INFO_SECTION="DevRelease$OS_BUILD_VERSION-Info"
+	INFO_SECTION="DevRelease-$OS_BUILD_VERSION"
 else
-	INFO_SECTION="Info"
+	INFO_SECTION="$OS_BUILD_VERSION"
 fi
+
+MAJOR_VERSION=`echo "$OS_VERSION" | cut -f 2 -d .`
 
 MESSAGE_PATH="/System/Library/Frameworks/Message.framework/Resources/Info.plist"
 MAIL_PATH="/Applications/Mail.app/Contents/Info.plist"
 
 if [[ -f $MESSAGE_PATH ]]; then
-	echo "Extracting Message plist"
-#	echo "Extraction path is: '$OS_VERSION-Message-$INFO_SECTION.plist'"
-	cp "$MESSAGE_PATH" "$OS_VERSION-Message-$INFO_SECTION.plist"
+	if [[ $MAJOR_VERSION < 9 ]]; then
+		echo "Extracting Message plist"
+		cp "$MESSAGE_PATH" "$OS_VERSION-message-$INFO_SECTION.plist"
+	fi
 fi
 
 if [[ -f $MAIL_PATH ]]; then
 	echo "Extracting Mail plist"
-#	echo "Extraction path is: '$OS_VERSION-Mail-$INFO_SECTION.plist'"
-	cp "$MAIL_PATH" "$OS_VERSION-Mail-$INFO_SECTION.plist"
+	cp "$MAIL_PATH" "$OS_VERSION-mail-$INFO_SECTION.plist"
 fi
